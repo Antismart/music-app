0c146c0a0d08ec403316ef072d1354dc642ba2fd
Antismart <timbwamoses83@gmail.com>
2022-11-28 10:50:19 +0300

    made the searchbar functional

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 function SearchBar() {
   const [query, setQuery]= useState("")
   return (
       <div className=" bg-black  rounded-full p-1 h-10">
-      <input className=' w-full h-10 rounded-full' placeholder='Search For Music' onChange={event => setQuery(event.target.value)} />
+      <input className=' w-full h-10 rounded-full ' placeholder='Search For Music' onChange={event => setQuery(event.target.value)} />
       </div>
   )
 }
